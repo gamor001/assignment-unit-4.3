f13f070a4244da0a44e717666b18f82a7503dad3
Brie Morris <morris.brie@gmail.com>
2023-06-22 20:59:07 -0500

    group work

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,15 +1,64 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+function addItem(item) {
+    basket.push(item);
+    console.log(`Now our basket has: ${basket}`);
+    return true;
+}
+addItem('gluten free pasta')
+addItem('salad')
+addItem('sweet potato')
+addItem('celery')
+
+function listItems() {
+   for(i=0; i< basket.length; i += 1){
+    console.log(`Item in basket: ${basket[i]}`);
+   } 
+}
+listItems()
 
 
+function empty() {
+   basket = [];
+}
+empty();
+console.log(`Our basket is empty ${basket}`);
 
+const maxItems = 5;
+function addItem(item) {
+    basket.push(item);
+    console.log(`Now our basket has: ${basket}`);
+        function isFull() {
+            if(basket.length < maxItems){
+                return true;
+            } else if(basket.length === maxItems) {
+                return false;
+            } 
+    }   
+}
+addItem('gummy bears')
+isFull(basket)
+addItem('pizza')
+addItem('apple')
+addItem('pear')
+addItem('heirloom tomato')
+addItem('bananas')
+// function isFull() {
+//     if(basket.length < maxItems){
+//         return false;
+//     } else if(basket.length === maxItems) {
+//         return true;
+//     }  
+// }
+// isFull();
+// console.log(isFull());
 
+function removeItem(item){
+    basket.indexOf(apple, 0); 
 
-
-
-
-
+}
 
 
 
